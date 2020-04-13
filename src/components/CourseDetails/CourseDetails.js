cd78800eab42e359f1c361d37222b9976cdd873d
Daniel Ionita <danielionita88@yahoo.com>
2020-04-12 19:07:02 -0500

    CourseDetails styling and conditional rendering

--- src/components/CourseDetails/CourseDetails.js
+++ src/components/CourseDetails/CourseDetails.js
@@ -1,24 +1,31 @@
 import React from 'react';
+import StarRatings from 'react-star-ratings';
+import classes from './CourseDetails.module.css'
 
 const CourseDetails = props => {
     
     const id = parseInt(props.match.params.id)
     const currentCourse = props.courses.find(course => course.courseId === id)
     console.log(currentCourse)
-    
+
     return(
-        <div>
-            <h1>{currentCourse.title}</h1>
+        <div className={classes.Details}>
+            <h1><a href={currentCourse.url}>{currentCourse.title}</a></h1>
             <img src={currentCourse.imgUrl}/>
             <h2>Course info:</h2>
-            <a href={currentCourse.url}>Link</a>
-            <p>Rating: {currentCourse.providerRatings} </p>
-            <p>Duration: {`${currentCourse.duration} ${currentCourse.durationPeriod}`}</p>
-            <p>Program type: {currentCourse.programType} </p>
-            <p>Level: {currentCourse.level}</p>
-            <h3>Description: </h3>
+            <a href={currentCourse.url}>Course Link</a>
+            <p><strong>Rating: </strong>&nbsp;
+                <StarRatings 
+                    numberOfStart={5} 
+                    starDimension='20px'
+                    starRatedColor='gold'
+                    rating={currentCourse.providerRatings ? currentCourse.providerRatings : 0}
+                /> {currentCourse.providerRatings ? (currentCourse.providerRatings) : null}</p>
+            <p><strong>Duration: </strong> {currentCourse.duration ? `${currentCourse.duration} ${currentCourse.durationPeriod}` : 'Unknown'}</p>
+            <p><strong>Program type: </strong>{currentCourse.programType} </p>
+            <p><strong>Level: </strong>{currentCourse.level ? currentCourse.level : 'Unknown' }</p>
+            <h3><strong>Description: </strong></h3>
             <p>{currentCourse.shortDescription}</p>
-
         </div>
     );
 }
